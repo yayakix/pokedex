84937f85331e57a1b0d9cb7d656d8690392eaa9c
Iyana Marquez <yayakix@gmail.com>
2022-05-19 17:28:51 -0400

    post method

--- server.js
+++ server.js
@@ -5,31 +5,32 @@ const pokemon = require("./models/pokemon");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
-
 const methodOverride = require("method-override");
-
 app.use(methodOverride("_method"));
+// standard installs
 
+app.get("/pokemon/new", (req, res) => {
+  res.render("new.ejs");
+});
 
 app.get('/pokemon', (req,res) => {
     res.render('index.ejs', {pokemon : pokemon})
-
 })
 
 app.get('/pokemon/:id', (req,res) => {
 res.render("show.ejs", { pokemon: pokemon[req.params.id] });
 })
+// shows individual pokemon stat pages
 
-app.get("/pokemon/new", (req, res) => {
-
-});
 
 app.get("/pokemon/:id/edit", (req, res) => {
 
 });
 
 app.post("/pokemon", (req, res) => {
-
+    pokemon.push(req.body)
+    console.log(req.body)
+res.redirect('/pokemon')
 });
 
 app.put("/pokemon/:id", (req, res) => {
