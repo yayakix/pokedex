fc3bdf4ea93bfb03d1e960c3c76efaecaa60f688
Iyana Marquez <yayakix@gmail.com>
2022-05-19 21:36:18 -0400

    objects converted to string in edit page

--- server.js
+++ server.js
@@ -13,37 +13,59 @@ app.get("/pokemon/new", (req, res) => {
   res.render("new.ejs");
 });
 
-app.get('/pokemon', (req,res) => {
-    res.render('index.ejs', {pokemon : pokemon})
-})
+app.get("/pokemon", (req, res) => {
+  res.render("index.ejs", { pokemon: pokemon });
+});
 
-app.get('/pokemon/:id', (req,res) => {
-res.render("show.ejs", { pokemon: pokemon[req.params.id] });
-})
+app.get("/pokemon/:id", (req, res) => {
+
+  res.render("show.ejs", { pokemon: pokemon[req.params.id] });
+});
 // shows individual pokemon stat pages
-
 
 app.get("/pokemon/:id/edit", (req, res) => {
 
+  let stat = pokemon[req.params.id].stats;
+
+  var strBuilder = [];
+  for (key in stat) {
+    if (stat.hasOwnProperty(key)) {
+      strBuilder.push(key + ": " + stat[key]);
+    }
+  }
+  console.log(strBuilder);
+  stat = strBuilder;
+
+
+
+  res.render("edit.ejs", {
+    pokemon: pokemon[req.params.id],
+    index: req.params.id,
+    stats: stat
+  });
 });
 
 app.post("/pokemon", (req, res) => {
-    pokemon.push(req.body)
-    console.log(req.body)
-res.redirect('/pokemon')
+  let stat = req.body.stats;
+
+  var obj = JSON.parse(stat);
+  console.log(obj);
+
+  pokemon.push(req.body);
+  console.log(req.body);
+  res.redirect("/pokemon");
 });
 
 app.put("/pokemon/:id", (req, res) => {
-
+  pokemon[req.params.id] = req.body;
+  res.redirect("/pokemon");
 });
 
 app.delete("/pokemon/:id", (req, res) => {
-
+  pokemon.splice(req.params.id, 1); //remove the item from the array
+  res.redirect("/pokemon");
 });
 
-
-
-
 app.listen(3000, (req, res) => {
-    console.log('server is running on port 3000')
-})
+  console.log("server is running on port 3000");
+});
